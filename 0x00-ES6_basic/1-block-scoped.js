86bd89f2a1fd86b7319ef96945079edd5a526893
Needtofnf <cedricthulani9@gmail.com>
2022-12-20 23:22:28 -0800

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
+    var task = true;
     let task2 = false;
   }
 
