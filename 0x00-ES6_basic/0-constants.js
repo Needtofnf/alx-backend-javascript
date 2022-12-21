a98815261d13f1255f9075576ebcf71c8c2f4154
Needtofnf <cedricthulani9@gmail.com>
2022-12-20 23:27:25 -0800

    0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,4 @@
-export function taskFirst() {
-  const task = 'I prefer const when I can.';
-  return task;
-}
+import { taskFirst, taskNext } from './0-constants.js';
 
-export function getLast() {
-  return ' is okay';
-}
+console.log(`${taskFirst(const)} ${taskNext(let:)}`);
 
-export function taskNext() {
-  let combination = 'But sometimes let';
-  combination += getLast();
-
-  return combination;
-}
